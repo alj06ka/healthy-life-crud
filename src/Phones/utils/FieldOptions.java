9ffd781424daec7ca4b1a6442f59caf6b8a4cc52
Alexey Yurkevich <Alexey.Yurkevich1@gmail.com>
2019-04-11 21:17:12 +0300

    Added field options

---
+++ src/Phones/utils/FieldOptions.java
@@ -0,0 +1,70 @@
+package Phones.utils;
+
+import java.lang.reflect.Method;
+
+public class FieldOptions {
+
+
+    public enum FieldType {
+        LIST,
+        OBJECT,
+        BOOLEAN,
+        TEXT
+    }
+    private String name;
+
+    private Object fieldValue;
+    private Class<?> classType;
+    private Method get, set;
+
+    public FieldOptions(String name) {
+        this.name = name;
+    }
+
+    public String getFieldName() {
+        if (get.isAnnotationPresent(FieldName.class)) {
+            return get.getAnnotation(FieldName.class).value();
+        }
+        return getName();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Method getGet() {
+        return get;
+    }
+
+    public void setGet(Method get) {
+        this.get = get;
+    }
+
+    public Method getSet() {
+        return set;
+    }
+
+    public void setSet(Method set) {
+        this.set = set;
+    }
+
+    public Object getFieldValue() {
+        return fieldValue;
+    }
+
+    public void setFieldValue(Object fieldValue) {
+        this.fieldValue = fieldValue;
+    }
+
+    public Class<?> getClassType() {
+        return classType;
+    }
+
+    public void setClassType(Class<?> classType) {
+        this.classType = classType;
+    }
+}
