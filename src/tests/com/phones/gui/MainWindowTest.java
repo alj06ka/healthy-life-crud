c46a81c7fd2e13ea1d1ac4ed0b273df622eb9093
Alexey Yurkevich <Alexey.Yurkevich1@gmail.com>
2019-04-24 07:22:02 +0300

    Add basic gui test

---
+++ src/tests/com/phones/gui/MainWindowTest.java
@@ -0,0 +1,40 @@
+package com.phones.gui;
+
+
+import com.phones.phones.AbstractPhone;
+import com.phones.phones.mobilePhones.SmartPhone;
+import com.phones.utils.ExtractFields;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.api.FxAssert;
+import org.testfx.api.FxRobot;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.Start;
+import org.testfx.matcher.control.LabeledMatchers;
+
+
+@ExtendWith(ApplicationExtension.class)
+class MainWindowTest{
+
+    private Button button;
+
+    @Start
+    public void start(Stage stage) {
+        stage.setTitle("Phone editor test title");
+        AbstractPhone phone = new SmartPhone();
+        VBox vBox = new ExtractFields(phone);
+        vBox.getChildren().add(button);
+        Scene scene = new Scene(vBox);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @Test
+    void should_contain_button_with_text(FxRobot robot) {
+        FxAssert.verifyThat(this.button, LabeledMatchers.hasText("Click me"));
+    }
+}
