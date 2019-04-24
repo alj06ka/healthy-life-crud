37f10d23845ad9640dc4a95fd17391379940f7a9
Alexey Yurkevich <Alexey.Yurkevich1@gmail.com>
2019-04-24 07:13:35 +0300

    Add enum field support

---
+++ src/com/phones/gui/fields/SelectFieldEditor.java
@@ -0,0 +1,89 @@
+package com.phones.gui.fields;
+
+import com.phones.utils.FieldOptions;
+import com.phones.utils.LocalizedName;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.Node;
+import javafx.scene.control.ComboBox;
+
+import java.lang.reflect.Field;
+
+/**
+ * Class for handling Enum fields in a gui
+ */
+public class SelectFieldEditor extends FieldWrapper {
+
+    private ComboBox<ComboBoxItem> comboBox;
+    private ObservableList<ComboBoxItem> enumFields;
+
+    public SelectFieldEditor(Object objectToInspect, FieldOptions field) {
+        comboBox = new ComboBox<>();
+        this.fieldObject = objectToInspect;
+        this.fieldOptions = field;
+
+        // Only Enum fields are supported
+        // To-Do: add list support
+        enumFields = FXCollections.observableArrayList();
+        if (field.getClassType().isEnum()) {
+            Enum[] enumValues = (Enum[]) fieldOptions.getClassType().getEnumConstants();
+            for (Enum enumValue : enumValues) {
+                enumFields.add(new ComboBoxItem(enumValue));
+            }
+            comboBox.setItems(enumFields);
+            Object ObjectValue = getValueFromObject(this.fieldObject);
+            writeObjectToElement(ObjectValue);
+        }
+    }
+
+    @Override
+    protected Object getValueFromElement() {
+        return comboBox.getValue().enumField;
+    }
+
+    @Override
+    protected void writeObjectToElement(Object objectToWrite) {
+        ComboBoxItem foundItem = null;
+        for (ComboBoxItem item : enumFields) {
+            if (item.getEnumField() == objectToWrite) {
+                foundItem = item;
+            }
+        }
+        comboBox.getSelectionModel().select(foundItem);
+    }
+
+    @Override
+    public Node getControl() {
+        return comboBox;
+    }
+
+    /**
+     * Class for describing combobox items and showing class
+     * names as field value for combobox.
+     */
+    private class ComboBoxItem {
+        private Enum enumField;
+
+        ComboBoxItem(Enum enumField) {
+            this.enumField = enumField;
+        }
+
+        Enum getEnumField() {
+            return enumField;
+        }
+
+        @Override
+        public String toString() {
+            Class<? extends Enum> enumType = enumField.getClass();
+            String localizedName = enumField.toString();
+            try {
+                Field enumConstant = enumType.getField(localizedName);
+                if (enumConstant.isAnnotationPresent(LocalizedName.class)) {
+                    localizedName = enumConstant.getAnnotation(LocalizedName.class).value();
+                }
+            } catch (NoSuchFieldException ignored) {
+            }
+            return localizedName;
+        }
+    }
+}
