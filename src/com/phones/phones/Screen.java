15157539ed0030f916dc076a93db000e03ada029
Alexey Yurkevich <Alexey.Yurkevich1@gmail.com>
2019-04-24 07:22:40 +0300

    Extract Screen class into new file

---
+++ src/com/phones/phones/Screen.java
@@ -0,0 +1,29 @@
+package com.phones.phones;
+
+import com.phones.utils.LocalizedName;
+
+public class Screen {
+    private int width;
+    private int height;
+
+    public Screen() {
+    }
+
+    @LocalizedName("Ширина")
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    @LocalizedName("Высота")
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+}
