3b285e7dfe1a40a11f76555e5831fc5a6f776efb
Alexey Yurkevich <Alexey.Yurkevich1@gmail.com>
2019-05-24 23:27:29 +0300

    Update text serializer

--- src/com/phones/serializers/TextSerializer.java
+++ src/com/phones/serializers/TextSerializer.java
@@ -2,109 +2,205 @@ package com.phones.serializers;
 
 import com.phones.utils.FieldOptions;
 import com.phones.utils.FieldsParser;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.*;
 
-class TextSerializer {
-
-    private static String handleFields(int tabLevel, Object objectToWrite, ArrayList<Object> parentObject) {
-        if (objectToWrite.getClass() != null) {
-            ObservableList<Object> parentObservable = FXCollections.observableArrayList(parentObject);
-            StringBuilder resultString = new StringBuilder();
-            ArrayList<FieldOptions> fields = FieldsParser.parseFields(objectToWrite);
-            for (FieldOptions field : fields) {
-                String className = field.getClassType().getName();
-                String fieldName = field.getName();
-                if (field.getFieldType() == FieldOptions.FieldType.OBJECT) {
-                    resultString.append(addTabs(tabLevel)).append("<object ").append(fieldName).append(" ").append(className).append(">\n");
-                    resultString.append(addTabs(tabLevel + 1)).append(parentObservable.indexOf(field.getFieldValue()));
-                    resultString.append("\n").append(addTabs(tabLevel)).append("</object>\n");
-                } else {
-                    resultString.append(addTabs(tabLevel)).append("<").append(fieldName).append(" ").append(field.getFieldValue()).append(">\n");
-                }
-            }
-            return resultString.toString();
-        }
-        return "null";
-    }
-
-    private static String addTabs(int tabLevel) {
-        return "\t".repeat(tabLevel);
-    }
-
-    public void serialize(ArrayList<Object> objectToWrite, OutputStream outputStream) throws IOException {
-        try (OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream)) {
-            writeObject(objectToWrite, outputStreamWriter);
+public class TextSerializer implements Serializable {
+    private int nestingLevel = 0;
+    @Override
+    public void serialize(Object o, OutputStream outputStream) {
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream, "UTF-8");
+            nestingLevel = 0;
+            writeObject(o, outputStreamWriter);
+            outputStreamWriter.flush();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
-    public ArrayList<Object> deserialize(InputStream inputStream) throws IOException {
-        Scanner scanner = new Scanner(inputStream);
-        String inputText = removeTabs(scanner.next());
-        List<String> inputLineList = Arrays.asList(inputText.split("\n"));
-        String line = inputLineList.get(0);
-        while (line != null){
-            Class classObject;
-            Object object;
-            if (getRecordType(line).equals("object")) {
-                try {
-                    classObject = Class.forName(getClassName(line));
-                    object = classObject.newInstance();
-//                    setFields(classObject, object);
-                } catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
-                    e.printStackTrace();
-                }
-            }
+    @Override
+    public Object deserialize(InputStream inputStream) {
+        Scanner scanner = new Scanner(inputStream, "UTF-8").useDelimiter("\\A");
+        String inputString = clearWhitespaces(scanner.next());
+        Object result;
+        try {
+            result = readObject(inputString);
+            return result;
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        return readObject();
-    }
-
-    private String getClassName(String line) {
-        String[] strings = line.split(" ");
-        String className = strings[2];
-        className = className.replace(">", "");
-        return className;
-    }
-
-    private String getRecordType(String line) {
-        String[] strings = line.split(" ");
-        String className = strings[0];
-        className = className.replace("<", "");
-        className = className.replace("\t", "");
-        return className;
-    }
-
-    private String removeTabs(String fileInput) {
-        return fileInput.replace("\t", "");
-    }
-
-    private void writeObject(ArrayList<Object> objectToWrite, OutputStreamWriter outputStreamWriter) throws IOException {
-        writeLine(outputStreamWriter, "<list " + objectToWrite.getClass() + ">\n");
-        for (Object itemObject : objectToWrite) {
-            writeLine(outputStreamWriter, addTabs(1) + "<object item ");
-            writeLine(outputStreamWriter, itemObject.getClass().getName() + ">\n");
-            writeLine(outputStreamWriter, handleFields(2, itemObject, objectToWrite));
-            writeLine(outputStreamWriter, addTabs(1) + "</object>\n");
-        }
-        writeLine(outputStreamWriter, "</list>");
-
-    }
-
-    private ArrayList<Object> readObject() throws IOException {
-        ArrayList<Object> streamObject;
         return null;
     }
 
-    private void writeLine(OutputStreamWriter outputStreamWriter, String stringToWrite) throws IOException {
-        outputStreamWriter.append(stringToWrite);
+    private void writeObject(Object object, OutputStreamWriter outputStreamWriter) throws IOException {
+
+        appendFormat(outputStreamWriter, "{");
+        nestingLevel++;
+
+        if (object == null) {
+            appendFormat(outputStreamWriter, "null");
+        } else {
+            String className = object.getClass().getName();
+            FieldOptions.FieldType fieldType = FieldsParser.getFieldType(object.getClass());
+            String header = fieldType.toString() + ":" + className + ":";
+            switch (fieldType) {
+                case TEXT:
+                    appendFormat(outputStreamWriter, header + "\"" + object.toString() + "\"");
+                    break;
+                case LIST:
+                    appendFormat(outputStreamWriter, header);
+                    nestingLevel++;
+                    appendFormat(outputStreamWriter, "[");
+                    nestingLevel++;
+                    for (Object listItem : (List) object) {
+                        writeObject(listItem, outputStreamWriter);
+                    }
+                    nestingLevel--;
+                    appendFormat(outputStreamWriter, "]");
+                    nestingLevel--;
+                    break;
+                case OBJECT:
+                    appendFormat(outputStreamWriter, header);
+                    nestingLevel++;
+                    appendFormat(outputStreamWriter, "[");
+                    nestingLevel++;
+                    ArrayList<FieldOptions> fields = FieldsParser.parseFields(object);
+                    for (FieldOptions field : fields) {
+                        appendFormat(outputStreamWriter, field.getName() + ":");
+                        writeObject(field.getFieldValue(), outputStreamWriter);
+                    }
+                    nestingLevel--;
+                    appendFormat(outputStreamWriter, "]");
+                    nestingLevel--;
+                    break;
+            }
+        }
+        nestingLevel--;
+        appendFormat(outputStreamWriter, "}");
+    }
+
+    private Object readObject(String inputString) throws InstantiationException, IllegalAccessException, ClassNotFoundException, NoSuchMethodException, InvocationTargetException {
+        if (inputString.equals("{null}")){
+            return null;
+        }
+        int startIndex = inputString.indexOf('{') + 1;
+        int endIndex = inputString.indexOf(':', startIndex);
+        String inputStringName = inputString.substring(startIndex, endIndex);
+        FieldOptions.FieldType fieldType = FieldOptions.FieldType.valueOf(inputStringName);
+
+        startIndex = endIndex + 1;
+        endIndex = inputString.indexOf(':', startIndex);
+        String className = inputString.substring(startIndex, endIndex);
+        startIndex = endIndex + 1;
+        switch (fieldType) {
+            case TEXT:
+                endIndex = inputString.indexOf('"', startIndex + 1);
+                String value = inputString.substring(startIndex + 1, endIndex);
+                Class<?> objectClass = Class.forName(className);
+                if (objectClass != String.class) {
+                    Method valueOf = objectClass.getMethod("valueOf", String.class);
+                    return valueOf.invoke(null, value);
+                } else {
+                    return value;
+                }
+            case LIST:
+                ArrayList<Object> list = new ArrayList<>();
+                endIndex = getBlockEnd(inputString, startIndex, '[', ']');
+                String listBlock = inputString.substring(startIndex + 1, endIndex);
+                startIndex = 0;
+                while ((startIndex = listBlock.indexOf("{", startIndex)) != -1) {
+                    endIndex = getBlockEnd(listBlock, startIndex, '{', '}');
+                    list.add(readObject(listBlock.substring(startIndex, endIndex)));
+                    startIndex = endIndex + 1;
+                }
+                return list;
+            case OBJECT:
+                startIndex = endIndex + 1;
+                endIndex = getBlockEnd(inputString, startIndex, '[', ']');
+                String objectBlock = inputString.substring(startIndex + 1, endIndex);
+                endIndex = 0;
+                HashMap<String, Object> fields = new HashMap<>();
+                while ((startIndex = objectBlock.indexOf(':', endIndex)) != -1) {
+                    String fieldName = objectBlock.substring(endIndex, startIndex);
+                    startIndex++;
+                    endIndex = getBlockEnd(objectBlock, startIndex, '{', '}');
+                    Object fieldValue = readObject(objectBlock.substring(startIndex, endIndex + 1));
+                    if (fieldValue != null) {
+                        fields.put(fieldName, fieldValue);
+                    }
+                    endIndex++;
+                }
+                return constructObject(className, fields);
+            default:
+                return null;
+        }
+    }
+
+    private static Object constructObject(String className, Map<String, Object> fields) throws ClassNotFoundException, IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
+        Class<?> classDefinition = Class.forName(className);
+        Object o = classDefinition.newInstance();
+        for (Map.Entry<String, Object> entry : fields.entrySet()){
+            String fieldName = entry.getKey();
+            Object fieldValue = entry.getValue();
+            String setterName = "set" + fieldName;
+            Class<?> fieldType = fieldValue.getClass();
+            Method setterMethod;
+            try {
+                setterMethod = classDefinition.getMethod(setterName, fieldType);
+            } catch (NoSuchMethodException e){
+                if (FieldsParser.isBoxed(fieldType)) {
+                    setterMethod = classDefinition.getMethod(setterName, FieldsParser.getBoxedType(fieldType));
+                } else {
+                    throw e;
+                }
+            }
+            setterMethod.invoke(o, entry.getValue());
+        }
+        return o;
+    }
+
+    private static int getBlockEnd(String inputString, int startIndex, char startChar, char endChar) {
+        int nestingLevel = 1;
+        int i = startIndex + 1;
+        while (nestingLevel != 0) {
+            if (inputString.charAt(i) == startChar) {
+                nestingLevel++;
+            } else if (inputString.charAt(i) == endChar) {
+                nestingLevel--;
+            }
+            i++;
+        }
+        return i - 1;
+    }
+
+    private static String clearWhitespaces(String inputString){
+        StringBuilder result = new StringBuilder(inputString);
+        boolean isStr = false;
+        for (int i = 0; i < result.length(); i++){
+            char currentChar = result.charAt(i);
+            if (currentChar == '"'){
+                isStr = !isStr;
+            } else if (Character.isWhitespace(currentChar) && !isStr){
+                result.deleteCharAt(i);
+                i--;
+            }
+        }
+
+        return result.toString();
+    }
+
+    private void appendFormat(OutputStreamWriter out, String s) throws IOException{
+        for (int i = 0; i < nestingLevel; i++){
+            out.append('\t');
+        }
+        out.append(s);
+        out.append('\n');
     }
 }
